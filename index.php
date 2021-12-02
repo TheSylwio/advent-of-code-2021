3e006b69c670b24d2f6acd5c212aba9f15bcd7fd
TheSylwio <thesylwio@gmail.com>
2021-12-02 11:35:25 +0100

    Add day 2 task

--- index.php
+++ index.php
@@ -4,8 +4,14 @@ namespace App;
 
 require 'vendor/autoload.php';
 
+use App\Interfaces\TaskInterface;
 use App\Tasks\Task_01;
+use App\Tasks\Task_02;
 
-$task1 = new Task_01();
-$task1->executeFirstPart();
-$task1->executeSecondPart();
+/** @var TaskInterface[] $tasks */
+$tasks = [new Task_01(), new Task_02()];
+
+foreach ($tasks as $task) {
+	$task->executeFirstPart();
+	$task->executeSecondPart();
+}
---
+++ src/tasks/Task_02.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Tasks;
+
+use App\Interfaces\TaskInterface;
+
+class Task_02 extends BaseTask implements TaskInterface {
+	/** @var array|string[][] */
+	private array $commands;
+
+	public function __construct() {
+		parent::__construct();
+		$this->commands = array_map(fn($command) => explode(' ', $command), $this->getInput());
+	}
+
+	public function executeFirstPart() {
+		$position = 0;
+		$depth = 0;
+
+		foreach ($this->commands as $command) {
+			switch ($command[0]) {
+				case 'forward':
+					$position += $command[1];
+					break;
+				case 'up':
+					$depth -= $command[1];
+					break;
+				case 'down':
+					$depth += $command[1];
+			}
+		}
+
+		$result = $position * $depth;
+		echo "Day 02 ~ Part 1 | Result: $result \r\n";
+	}
+
+	public function executeSecondPart() {
+		$position = 0;
+		$depth = 0;
+		$aim = 0;
+
+		foreach ($this->commands as $command) {
+			switch ($command[0]) {
+				case 'forward':
+					$position += $command[1];
+					$depth += $aim * $command[1];
+					break;
+				case 'up':
+					$aim -= $command[1];
+					break;
+				case 'down':
+					$aim += $command[1];
+			}
+		}
+
+		$result = $position * $depth;
+		echo "Day 02 ~ Part 2 | Result: $result \r\n";
+	}
+}
