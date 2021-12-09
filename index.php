2b3d812fcd7a931a8bf4cbf79268c2667a638a19
TheSylwio <sylwester.gorol1@gmail.com>
2021-12-09 17:47:43 +0100

    Add day 7 part 2 task

--- index.php
+++ index.php
@@ -11,9 +11,10 @@ use App\Tasks\Task_03;
 use App\Tasks\Task_04;
 use App\Tasks\Task_05;
 use App\Tasks\Task_06;
+use App\Tasks\Task_07;
 
 /** @var TaskInterface[] $tasks */
-$tasks = [new Task_01(), new Task_02(), new Task_03(), new Task_04(), new Task_05(), new Task_06()];
+$tasks = [new Task_01(), new Task_02(), new Task_03(), new Task_04(), new Task_05(), new Task_06(), new Task_07()];
 
 foreach ($tasks as $task) {
 	$task->executeFirstPart();
--- src/Tasks/Task_07.php
+++ src/Tasks/Task_07.php
@@ -6,21 +6,25 @@ use App\Interfaces\TaskInterface;
 
 class Task_07 extends BaseTask implements TaskInterface {
 	public function executeFirstPart() {
-		[$input] = $this->getInput();
-		$fuels = array_map('intval', explode(',', $input));
-		$minFuel = $this->getMinFuel($fuels);
-
+		$minFuel = $this->getMinFuel();
 		$this->displayResult($minFuel);
 	}
 
 	public function executeSecondPart() {
-		// TODO: Implement executeSecondPart() method.
+		$minFuel = $this->getMinFuel(true);
+		$this->displayResult($minFuel);
 	}
 
-	private function getMinFuel(array $fuels): int {
-		return array_reduce($fuels, function ($acc, $fuel) use ($fuels) {
-			$fuelSum = array_reduce($fuels, function ($acc2, $item) use ($fuel) {
-				return $acc2 + abs($fuel - $item);
+	private function getMinFuel(bool $withSum = false): int {
+		[$input] = $this->getInput();
+		$fuels = array_map('intval', explode(',', $input));
+
+		$fuelRange = $withSum ? range(1, max($fuels)) : $fuels;
+
+		return array_reduce($fuelRange, function ($acc, $fuel) use ($withSum, $fuels) {
+			$fuelSum = array_reduce($fuels, function ($acc2, $item) use ($withSum, $fuel) {
+				$diff = abs($fuel - $item);
+				return $withSum ? $acc2 + array_sum(range(0, $diff)) : $acc2 + $diff;
 			});
 			return min($fuelSum, $acc);
 		}, PHP_INT_MAX);
