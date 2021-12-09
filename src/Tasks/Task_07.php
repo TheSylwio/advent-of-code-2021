535139923adb3f9bd72354790067f3c69c9fcdd4
TheSylwio <sylwester.gorol1@gmail.com>
2021-12-09 13:43:28 +0100

    Refactor day 7 part 1 task

--- src/Tasks/Task_07.php
+++ src/Tasks/Task_07.php
@@ -8,22 +8,21 @@ class Task_07 extends BaseTask implements TaskInterface {
 	public function executeFirstPart() {
 		[$input] = $this->getInput();
 		$fuels = array_map('intval', explode(',', $input));
-		$minFuel = null;
-
-		foreach ($fuels as $fuel) {
-			$sum = array_reduce($fuels, function ($acc, $item) use ($fuel) {
-				return $acc + (max($fuel, $item) - min($fuel, $item));
-			});
-
-			if (is_null($minFuel) || $sum < $minFuel) {
-				$minFuel = $sum;
-			}
-		}
+		$minFuel = $this->getMinFuel($fuels);
 
 		$this->displayResult($minFuel);
 	}
 
 	public function executeSecondPart() {
 		// TODO: Implement executeSecondPart() method.
+	}
+
+	private function getMinFuel(array $fuels): int {
+		return array_reduce($fuels, function ($acc, $fuel) use ($fuels) {
+			$fuelSum = array_reduce($fuels, function ($acc2, $item) use ($fuel) {
+				return $acc2 + abs($fuel - $item);
+			});
+			return min($fuelSum, $acc);
+		}, PHP_INT_MAX);
 	}
 }
