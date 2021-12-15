7f9ad0bfeb86278fd1c2567064e7994d19db4adb
TheSylwio <thesylwio@gmail.com>
2021-12-15 23:38:25 +0100

    Add day 10 part 2 task

--- src/Tasks/Task_10.php
+++ src/Tasks/Task_10.php
@@ -8,6 +8,7 @@ class Task_10 extends BaseTask implements TaskInterface {
 	public function executeFirstPart() {
 		$rows = $this->getInput(trimmed: true);
 		$illegalCharacters = [];
+
 		foreach ($rows as $row) {
 			$characters = str_split($row);
 			$openingBrackets = [];
@@ -17,8 +18,8 @@ class Task_10 extends BaseTask implements TaskInterface {
 					$openingBrackets[] = $character;
 					continue;
 				}
-				$lastOpeningBracket = array_pop($openingBrackets);
 
+				$lastOpeningBracket = array_pop($openingBrackets);
 				$areMatchingBrackets = $this->areMatchingBrackets($lastOpeningBracket, $character);
 
 				if (!$areMatchingBrackets) {
@@ -27,8 +28,82 @@ class Task_10 extends BaseTask implements TaskInterface {
 				}
 			}
 		}
+
 		$syntaxErrorScore = $this->getSyntaxErrorScore($illegalCharacters);
 		$this->displayResult($syntaxErrorScore);
+	}
+
+	public function executeSecondPart() {
+		$rows = $this->getInput(trimmed: true);
+		$incompleteRows = $this->getIncompleteRows($rows);
+		$completionCharacters = [];
+
+		foreach ($incompleteRows as $row) {
+			$characters = str_split($row);
+			$openingBrackets = [];
+
+			foreach ($characters as $character) {
+				if ($this->isOpeningBracket($character)) {
+					$openingBrackets[] = $character;
+					continue;
+				}
+
+				$lastOpeningBracket = array_pop($openingBrackets);
+				$areMatchingBrackets = $this->areMatchingBrackets($lastOpeningBracket, $character);
+
+				if (!$areMatchingBrackets) {
+					break;
+				}
+			}
+
+			$completionCharacters[] = array_reverse($openingBrackets);
+		}
+
+		$completionPoints = array_map(function ($completion) {
+			return $this->getCompletionPoints($completion);
+		}, $completionCharacters);
+		sort($completionPoints);
+		$middleScore = $completionPoints[floor(count($completionPoints) / 2)];
+
+		$this->displayResult($middleScore);
+	}
+
+	private function getCompletionPoints(array $completion): int {
+		return array_reduce($completion, function ($total, $character) {
+			return $total * 5 + $this->getCharacterPoint($character);
+		}, 0);
+	}
+
+	private function getCharacterPoint(string $character): int {
+		return match ($character) {
+			'(' => 1,
+			'[' => 2,
+			'{' => 3,
+			'<' => 4,
+		};
+	}
+
+	private function getIncompleteRows(array $rows): array {
+		return array_filter($rows, function (string $row) {
+			$characters = str_split($row);
+			$openingBrackets = [];
+
+			foreach ($characters as $character) {
+				if ($this->isOpeningBracket($character)) {
+					$openingBrackets[] = $character;
+					continue;
+				}
+
+				$lastOpeningBracket = array_pop($openingBrackets);
+				$areMatchingBrackets = $this->areMatchingBrackets($lastOpeningBracket, $character);
+
+				if (!$areMatchingBrackets) {
+					return false;
+				}
+			}
+
+			return true;
+		});
 	}
 
 	private function getSyntaxErrorScore(array $illegalCharacters): int {
@@ -55,9 +130,5 @@ class Task_10 extends BaseTask implements TaskInterface {
 
 	private function isOpeningBracket(string $bracket): bool {
 		return in_array($bracket, ['(', '[', '{', '<']);
-	}
-
-	public function executeSecondPart() {
-		// TODO: Implement executeSecondPart() method.
 	}
 }
