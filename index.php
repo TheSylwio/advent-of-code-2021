63d48c13fdaef3ed62687feddbea14cf4d3636bd
TheSylwio <thesylwio@gmail.com>
2021-12-16 00:40:36 +0100

    Remove entry task array

--- index.php
+++ index.php
@@ -4,19 +4,11 @@ namespace App;
 
 require 'vendor/autoload.php';
 
-use App\Interfaces\TaskInterface;
-use App\Tasks\Task_01;
-use App\Tasks\Task_02;
-use App\Tasks\Task_03;
-use App\Tasks\Task_04;
-use App\Tasks\Task_05;
-use App\Tasks\Task_06;
-use App\Tasks\Task_07;
-
-/** @var TaskInterface[] $tasks */
-$tasks = [new Task_01(), new Task_02(), new Task_03(), new Task_04(), new Task_05(), new Task_06(), new Task_07()];
-
-foreach ($tasks as $task) {
-	$task->executeFirstPart();
-	$task->executeSecondPart();
+for ($i = 1; $i <= 25; $i++) {
+	$className = 'App\Tasks\Task_' . sprintf("%02d", $i);
+	if (class_exists($className)) {
+		$task = new $className();
+		$task->executeFirstPart();
+		$task->executeSecondPart();
+	}
 }
